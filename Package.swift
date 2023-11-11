aac2fcb0019db9a9ecb73e5d879589acc62c2ade
Justin Ackermann <anomaddev@gmail.com>
2023-11-11 16:22:05 -0500

    added NomadFirebase

--- Package.swift
+++ Package.swift
@@ -10,6 +10,9 @@ let package = Package(
         .library(
             name: "NomadUtilities",
             targets: ["NomadUtilities"]),
+        .library(
+            name: "NomadFirebase",
+            targets: ["NomadFirebase"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -20,6 +23,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "NomadUtilities",
+            dependencies: []),
+        .target(
+            name: "NomadFirebase",
             dependencies: []),
         .testTarget(
             name: "NomadUtilitiesTests",
---
+++ Sources/NomadFirebase/NomadFirebase.swift
@@ -0,0 +1,12 @@
+//
+//  NomadFirebase.swift
+//
+//
+//  Created by Justin Ackermann on 11/11/23.
+//
+
+import Foundation
+
+class NomadFirebase {
+    
+}
