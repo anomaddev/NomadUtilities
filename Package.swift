dce1995cbad0a92f2b82834a3dc84b7d236ee853
Justin Ackermann <justinackermann@yahoo.com>
2023-11-23 23:45:46 -0500

    working

--- Package.swift
+++ Package.swift
@@ -23,10 +23,12 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "NomadUtilities",
-            dependencies: []),
+            dependencies: [],
+            path: "Sources/NomadUtilities"),
         .target(
             name: "NomadFirebase",
-            dependencies: []),
+            dependencies: ["NomadUtilities"],
+            path: "Sources/NomadFirebase"),
         .testTarget(
             name: "NomadUtilitiesTests",
             dependencies: ["NomadUtilities"]),
