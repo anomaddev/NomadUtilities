0565c517357aeec5b05e31425fb638bbb87a0946
Justin Ackermann <anomaddev@gmail.com>
2023-11-25 00:07:14 -0500

    working

--- Sources/NomadUtilities/Extensions/Numbers/Double+Utilities.swift
+++ Sources/NomadUtilities/Extensions/Numbers/Double+Utilities.swift
@@ -9,17 +9,31 @@ import Foundation
 
 extension Double {
     
-    /// outputs a `Double` as a `NSNumber`
-    public var number: NSNumber
-    { NSNumber(floatLiteral: self) }
+    /// is an Int?
+    var isInteger: Bool     { floor(self) == self }
     
-    /// outputs a `Double` as a `Int`
-    public var int: Int
-    { Int(self) }
+    /// Base Conversions
+    var int: Int            { Int(self) }
+    var float: Float        { Float(self) }
+    var cgfloat: CGFloat    { CGFloat(self) }
+    var number: NSNumber    { NSNumber(value: self) }
     
+    
+    // MARK: Unit Conversions
+    
+    /// Degrees to Radians
+    var radians: Double      { self * .pi / 180 }
+    /// Radians to Degrees
+    var degress: Double      { self * 180 / .pi }
+    
+    /// String representation
+    var string: String
+    { "\(self)" }
+    
+    // MARK: Transforms
     /// Rounds the `Double` to a given decimal places
     public func roundTo(places: Int,
-                 mode: FloatingPointRoundingRule! = .toNearestOrAwayFromZero) -> Double {
+                        mode: FloatingPointRoundingRule! = .toNearestOrAwayFromZero) -> Double {
         let divisor = pow(10.0, Double(places))
         return (self * divisor).rounded(mode) / divisor
     }
@@ -28,5 +42,11 @@ extension Double {
     public func truncate(places: Int) -> Double {
         let divisor = pow(10.0, Double(places))
         return (self * divisor).rounded(.towardZero) / divisor
+    }
+    
+    func HMS() -> (Double, Double, Double) {
+        let (hr,  minf) = modf(self / 3600)
+        let (min, secf) = modf(60 * minf)
+        return (hr, min, 60 * secf)
     }
 }
