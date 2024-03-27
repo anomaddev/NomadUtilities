a9b23a55b8be0aa68af29a34bca3097c5a5872ed
anomaddev <anomaddev@gmail.com>
2024-03-27 00:21:30 -0400

    working

--- Package.swift
+++ Package.swift
@@ -9,11 +9,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "NomadUtilities",
-            targets: ["NomadUtilities"]),
-        .library(
-            name: "NomadUI",
-            targets: ["NomadUI"]
-        ),
+            targets: ["NomadUtilities", "NomadUI"]),
         .library(
             name: "NomadFirebase",
             targets: ["NomadFirebase"]),
