886172fe8c7f83da7b8ba6f19880c59e9026a250
anomaddev <anomaddev@gmail.com>
2024-03-29 00:05:34 -0400

    working

--- Package.swift
+++ Package.swift
@@ -7,34 +7,20 @@ let package = Package(
     name: "NomadUtilities",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "NomadUtilities",
-            targets: ["NomadUtilities", "NomadUI"]),
-        .library(
-            name: "NomadFirebase",
-            targets: ["NomadFirebase"]),
+        .library(name: "NomadUtilities", targets: ["NomadUtilities"]),
+        .library(name: "NomadFirebase", targets: ["NomadFirebase"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/yeahdongcn/UIColor-Hex-Swift.git", from: "5.1.0"),
-        .package(url: "https://github.com/fahidattique55/FAPanels.git", .upToNextMajor(from: "2.0.0"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "NomadUtilities",
-            dependencies: [
-                .product(name: "UIColorHexSwift", package: "UIColor-Hex-Swift"),
-            ]),
-        .target(
-            name: "NomadUI",
-            dependencies: [
-                "NomadUtilities",
-                .product(name: "UIColorHexSwift", package: "UIColor-Hex-Swift"),
-                .product(name: "FAPanels", package: "FAPanels")
-            ]),
+            dependencies: []
+        ),
         .target(
             name: "NomadFirebase",
             dependencies: ["NomadUtilities"]),
--- Sources/NomadUI/Extensions/NomadUI+UIColor.swift
+++
@@ -1,8 +0,0 @@
-//
-//  NomadUI+UIColor.swift
-//
-//
-//  Created by Justin Ackermann on 3/26/24.
-//
-
-import UIKit
