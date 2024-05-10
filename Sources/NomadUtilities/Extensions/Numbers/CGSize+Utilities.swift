34abb4861ffee4e661ce5b4dc7e6151f8ca8d32c
anomaddev <anomaddev@gmail.com>
2024-05-09 21:57:07 -0400

    working

---
+++ Sources/NomadUtilities/Extensions/Numbers/CGSize+Utilities.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  
+//
+//  Created by Justin Ackermann on 5/9/24.
+//
+
+import UIKit
+
+extension CGSize {
+    
+    public static func square(_ size: CGFloat) -> CGSize {
+        return CGSize(width: size, height: size)
+    }
+    
+}
