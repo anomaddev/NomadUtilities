017f19460305a6dc4159c77df3e648d0830a4241
Justin Ackermann <anomaddev@gmail.com>
2024-06-13 20:55:54 -0400

    working

---
+++ Sources/NomadUtilities/Extensions/NomadUtilities+Sequence.swift
@@ -0,0 +1,39 @@
+//
+//  NomadUtilities+Sequence.swift
+//
+//
+//  Created by Justin Ackermann on 6/10/24.
+//
+
+import Foundation
+
+extension Sequence {
+    
+    /// Asynchronously maps the elements of the sequence to a new array
+    public func asyncMap<T>(
+        _ transform: (Element) async throws -> T
+    ) async rethrows -> [T] {
+        var values = [T]()
+
+        for element in self {
+            try await values.append(transform(element))
+        }
+
+        return values
+    }
+    
+    public func concurrentForEach(
+        _ operation: @escaping (Element) async -> Void
+    ) async {
+        // A task group automatically waits for all of its
+        // sub-tasks to complete, while also performing those
+        // tasks in parallel:
+        await withTaskGroup(of: Void.self) { group in
+            for element in self {
+                group.addTask {
+                    await operation(element)
+                }
+            }
+        }
+    }
+}
