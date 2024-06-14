2bfe555d622798d4ce3a7c95e78561458cab7ee1
Justin Ackermann <anomaddev@gmail.com>
2024-06-14 01:54:07 -0400

    working

--- Sources/NomadUtilities/Extensions/Views/NomadUtilities+UIImage.swift
+++ Sources/NomadUtilities/Extensions/Views/NomadUtilities+UIImage.swift
@@ -5,7 +5,9 @@
 //  Created by Justin Ackermann on 6/13/24.
 //
 
+// Core iOS
 import UIKit
+import AVFoundation
 
 extension UIImage {
     
@@ -43,6 +45,34 @@ extension UIImage {
         let newImage = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
         
-        return newImage
+        return newImage?.withRenderingMode(.alwaysTemplate)
+    }
+    
+    /// Resize image while keeping the aspect ratio. Original image is not modified.
+    /// - Parameters:
+    ///   - width: A new width in pixels.
+    ///   - height: A new height in pixels.
+    /// - Returns: Resized image.
+    public func resize(to size: CGSize) -> UIImage {
+        // Keep aspect ratio
+        let maxSize = CGSize(width: size.width, height: size.height)
+        
+        let availableRect = AVFoundation.AVMakeRect(
+            aspectRatio: self.size,
+            insideRect: .init(origin: .zero, size: maxSize)
+        )
+        let targetSize = availableRect.size
+        
+        // Set scale of renderer so that 1pt == 1px
+        let format = UIGraphicsImageRendererFormat()
+        format.scale = 1
+        let renderer = UIGraphicsImageRenderer(size: targetSize, format: format)
+        
+        // Resize the image
+        let resized = renderer.image { _ in
+            self.draw(in: CGRect(origin: .zero, size: targetSize))
+        }
+        
+        return resized.withRenderingMode(.alwaysTemplate)
     }
 }
